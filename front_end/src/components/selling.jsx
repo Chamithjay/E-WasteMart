53ab43a02770cf9aadc4fa7677b5cee550c1b7f6
Udara Wickramasinghe <65730273+WickyUdara@users.noreply.github.com>
2024-07-12 17:42:13 +0530

    selling complete

--- front_end/src/components/selling.jsx
+++ front_end/src/components/selling.jsx
@@ -23,6 +23,16 @@ function Selling(){
                             <textarea name="pDescription" className="form-control" id="" placeholder='Enter product title here'></textarea>
                         </div>
                         <div className="form-group">
+                            <label htmlFor="pTitle">Product Status</label>
+                            <select name="pStatus" id="" className="form-control">
+                                <option value="">Select Product Status</option>
+                                <option value="">All parts are working</option>
+                                <option value="">Some parts are working</option>
+                                <option value="">All parts are not working</option>
+                                <option value="">I don't know</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
                             <label htmlFor="pTitle">Quantity</label>
                             <input type="number" className="form-control" id="quantity" placeholder="Enter Product Quantity" name="pQuantity"/>
                         </div>
@@ -31,6 +41,10 @@ function Selling(){
                         <div className="form-group">
                             <label htmlFor="pTitle">Price $</label>
                             <input type="text" className="form-control" id="quantity" placeholder="Enter Product Price" name="pPrice"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="pTitle">Product Rating  (between 0 and 5)</label>
+                            <input type="range" className="form-control" id="quantity" placeholder="Enter Product Price" name="pRating" min="0" max="5" />
                         </div>
 
                         <button type="submit" className="btn btn-primary">List this Product</button>
