fa10a08667753cc7668bc670183781e4dd5c152b
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-07-30 17:52:42 +0530

    back end

--- back_end/index.js
+++ back_end/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors= require('cors');
-const mongodb = require('mongodb');
+const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'E-WasteMart'
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
